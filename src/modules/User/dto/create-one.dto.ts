a760a60545c0ace785e174b64949bc3ea14e604f
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-06 17:36:02 +0330

    refactor: change create-one.dto

--- src/modules/User/dto/create-one.dto.ts
+++ src/modules/User/dto/create-one.dto.ts
@@ -1,5 +1,12 @@
+import { Genders } from "../enum/gender.enum";
+
 export interface CreateOneDtoType {
+  firstName: string;
+  lastName: string;
   userName: string;
   email: string;
+  gender: Genders;
+  phone: string;
+  image: string;
   password: string;
 }
