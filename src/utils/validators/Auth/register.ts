c4849a0d9eca638f9007dcaf63f3b7a498fb2261
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-06 17:46:42 +0330

    commit

--- src/utils/validators/Auth/register.ts
+++ src/utils/validators/Auth/register.ts
@@ -1,10 +1,16 @@
-import { object, string } from "yup";
-import { userNamePattern } from "../../patterns/patterns";
+import { mixed, object, string } from "yup";
+import { phoneNumberPattern, userNamePattern } from "../../patterns/patterns";
+import { Genders } from "../../../modules/User/enum/gender.enum";
 
 const registerSchema = object().shape({
+  firstName: string().min(2),
+  lastName: string().min(2),
   userName: string().min(4).required().matches(userNamePattern),
   email: string().min(5).email().required(),
   password: string().min(8).required(),
+  gender: mixed().required().oneOf([Genders.FEMALE, Genders.MALE]),
+  phone: string().matches(phoneNumberPattern).required(),
+  image: string(),
 });
 
 export default registerSchema;
