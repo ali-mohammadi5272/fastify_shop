8be7ca4092b3bba1da65bcd0387d4a0697e724a6
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-06 17:45:03 +0330

    commit

--- src/utils/patterns/patterns.ts
+++ src/utils/patterns/patterns.ts
@@ -1,4 +1,11 @@
 const userNamePattern: RegExp = /^[aA-zZ]{1}[aA-zZ0-9]{2,}(_[aA-zZ0-9]+)*?$/;
 const emailPattern: RegExp = /^[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+$/;
+const phoneNumberPattern: RegExp = /^(0|\+98)?9\d{9}$/;
+const phoneNumberPrefixPattern: RegExp = /^(0|\+98)/;
 
-export { userNamePattern, emailPattern };
+export {
+  userNamePattern,
+  emailPattern,
+  phoneNumberPattern,
+  phoneNumberPrefixPattern,
+};
